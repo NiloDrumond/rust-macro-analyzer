badf9d9fd0d2e087594ef492b314386cf49cb562
Nilo Drumond <nilobdrumond@gmail.com>
2024-05-16 16:44:15 -0300

    guarantee data folder exists

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use github::clone_repos;
 use github::get_most_popular_repos;
 use lines_count::count_crates_source_lines;
 use state::ScraperState;
-use std::path::Path;
 use std::error::Error;
+use std::path::Path;
 use tokio::task::JoinHandle;
 
 #[macro_use]
@@ -16,8 +16,15 @@ pub mod github;
 pub mod lines_count;
 pub mod state;
 
+const DATA_PATH: &str = "./data";
+
+fn create_data_folder() {
+    std::fs::create_dir(DATA_PATH).unwrap();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    create_data_folder();
     let mut state = ScraperState::load().unwrap_or_default();
     let repos = get_most_popular_repos(&mut state).await?;
     let repos_path = clone_repos(&mut state, repos).await?;
